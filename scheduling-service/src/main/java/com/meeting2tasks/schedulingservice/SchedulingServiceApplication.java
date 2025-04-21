dae63b9798e7d574e979b40cfdd6ded07fb08adc
thuongthuongthuong <huuthuongaa1@gmail.com>
2025-04-21 10:33:17 +0700

    add file pom and main class for each service

---
+++ scheduling-service/src/main/java/com/meeting2tasks/schedulingservice/SchedulingServiceApplication.java
@@ -0,0 +1,11 @@
+package com.meeting2tasks.schedulingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingServiceApplication.class, args);
+    }
+}
