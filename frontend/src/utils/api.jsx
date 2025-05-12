bf389cd4bd6130df7c1ae7a8237f8e7f0f9a360a
Nhân Nguyễn Trọng <nhannguyentrong355@gmail.com>
2025-05-12 20:47:46 +0700

    update README, update call api for scheduling management and task service

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -2,4 +2,15 @@ import axiosInstance from "./axios";
 
 // ----------------------------------------------------------------------
 
+// Scheduling management
 export const getUserByPRojectID = (id) => axiosInstance.get(`http://localhost:8082/api/users/${id}`);
+export const getTaskByProjectID = (id) => axiosInstance.get(`http://localhost:8082/api/tasks/${id}`);
+export const getSprintByProjectID = (id) => axiosInstance.get(`http://localhost:8082/api/sprints/${id}`);
+export const getSprintDetails = (id) => axiosInstance.get(`http://localhost:8082/api/sprint/${id}`);
+
+// Task service 
+export const updateTask = (id, data) => axiosInstance.put(`http://localhost:8081/api/tasks/${id}`, data);
+export const deleteTask = (id) => axiosInstance.delete(`http://localhost:8081/api/tasks/${id}`);
+export const addTask = (id, data) => axiosInstance.post(`http://localhost:8081/api/tasks/sprint/${id}`, data);
+export const processMeetingNotes = (data) => axiosInstance.post(`http://localhost:8081/api/meeting-notes`, data);
+export const getTaskByMileStone = (id) => axiosInstance.get(`http://localhost:8081/api/tasks/milestone/${id}`);
